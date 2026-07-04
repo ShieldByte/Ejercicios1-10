0860625b75e476a10adb2d0235af050a9f8d9520
Pablo Alexander Alejos Macias <alexalejmaci@gmail.com>
2026-07-03 19:54:08 -0600

    Agregados numeros de control y soporte para archivos en test.py

--- 1/test.py
+++ 1/test.py
@@ -1,17 +1,24 @@
-from antlr4 import*
+import sys
+from antlr4 import *
 from ExprLexer import ExprLexer
 
+# Número de control: 21031117
+# Número de control: 22031047
 
-lexer = ExprLexer(InputStream(input("? ")))
+
+if len(sys.argv) > 1:
+    nombre_archivo = sys.argv[1]
+    lexer = ExprLexer(FileStream(nombre_archivo, encoding="utf-8"))
+else:
+    lexer = ExprLexer(InputStream(input("? ")))
 
 tokens = CommonTokenStream(lexer)
 tokens.fill()
-print(tokens)
 
 for token in tokens.tokens:
     print("Texto :", token.text)
     print("Linea :", token.line)
     print("Columna :", token.column)
     nombre_token = lexer.symbolicNames[token.type]
-    print("Tipo ", nombre_token)
-    print("----------------------")
+    print("Tipo  ", nombre_token)
+    print("-----------------------")
